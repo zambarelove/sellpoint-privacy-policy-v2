0d4aa435ecb47962a5e1b6815212fcbbd402261a
zambarelove <zambarelove@gmail.com>
2022-06-29 12:18:35 +0530

    Test Commit

---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: { 
+      fontSize: {
+        '10xl': ['100px', { lineHeight: '1' }],
+      },
+      /*
+
+      spacing: {
+        13: '130px',
+      },
+      fontSize: {
+        '10xl': ['18rem', { lineHeight: '1' }],
+      },
+      colors: {
+       
+
+        'hotpink': {
+          100: '#fff0f7',
+          200: '#ffd2e8',
+          300: '#ffc3e1',
+          400: '#ffb4d9',
+          500: '#ffa5d2',
+          600: '#ff96ca',
+          700: '#ff87c3',
+          800: '#ff78bb',
+          900: '#FF69B4',
+        }
+
+      } */
+    },
+  },
+  plugins: [],
+}
